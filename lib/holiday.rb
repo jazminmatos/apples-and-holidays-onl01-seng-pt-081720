257d5459ab54f23eee0082c9421b9a35222f51d9
Jazmin Matos <jmatos1993@gmail.com>
2020-08-28 16:43:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday_array = []
     puts season.to_s.capitalize! + ":"
     holidays.collect do |holiday, supplies| #holiday = individual holidays, supplies = array of supplies
-    binding.pry
+    #binding.pry
       if holiday.to_s.include?("_")
       #if holiday == :new_years || holiday == :fourth_of_july || holiday == :memorial_day
         holiday.to_s.split("_").each {|word| holiday_array << word}  #turn holiday from symbol to string, split the two words into separate strings, put into array
