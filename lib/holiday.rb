45e4262b72cdf1a983cd98ca9568d0981ca680b6
Jazmin Matos <jmatos1993@gmail.com>
2020-08-28 16:16:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,7 @@ def all_supplies_in_holidays(holiday_hash)
     holidays.collect do |holiday, supplies| #holiday = individual holidays, supplies = array of supplies
       if holiday == :new_years || holiday == :fourth_of_july || holiday == :memorial_day
         holiday_array << (holiday.to_s).split("_")  #turn holiday from symbol to string, split the two words into separate strings, put into array
-        holiday_array.collect do |festivity| 
+        holiday_array.collect do |festivity, supply| 
           capitalized_words << festivity.capitalize.join(" ") ####I DON'T UNDERSTAND WHY CAPITALIZE WON'T WOOOOORK!!!!!
         puts "  " + capitalized_words + ": " + supplies.join(", ") 
         end
